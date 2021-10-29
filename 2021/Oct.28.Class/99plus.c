a4cf90cceb30f8c24acfb3edd2d14c19c4950d6e
Taoning Ge <gtn1024o@outlook.com>
2021-10-29 21:17:22 +0800

    change

--- 2021/Oct.28.Class/99plus.c
+++ 2021/Oct.28.Class/99plus.c
@@ -1,31 +1,24 @@
 // COPYRIGHT: <a href="https://github.com/gtn1024">gtn1024</a>
 #include <stdio.h>
 
-void printSpace();
-
 int main(void)
 {
-  int i, j, k;
-  for (i = 1; i <= 9; i++)
+  for (int i = 1; i <= 9; i++)
   {
-    // print space
-    for (j = 1; j < i; j++)
+    // print spaces
+    for (int j = 1; j < i; j++)
     {
-      printSpace();
+      // print one space
+      for (int k = 0; k < 7; k++)
+      {
+        printf(" ");
+      }
     }
-    for (j = 9, k = i; j >= i; j--, k++)
+    for (int j = 9, k = i; j >= i; j--, k++)
     {
       printf("%i+%i=%2i ", i, k, i + k);
     }
     printf("\n");
   }
   return 0;
-}
-
-void printSpace()
-{
-  for (int i = 0; i < 7; i++)
-  {
-    printf(" ");
-  }
 }
