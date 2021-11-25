8fdcefd60ce577bc4864279be51e3cd6eade69bc
Taoning Ge <gtn1024o@outlook.com>
2021-11-25 10:25:01 +0800

    update

--- 2021/Nov.25.Class/exp4_2.c
+++ 2021/Nov.25.Class/exp4_2.c
@@ -7,16 +7,11 @@ void printMap(int arr[], int);
 
 int main(void)
 {
-  int arr[1000];
   srand(time(NULL));
-  for (int i = 0; i < 1000; i++)
-  {
-    arr[i] = rand() % 10000;
-  }
   int count[10001] = {0};
   for (int i = 0; i < 1000; i++)
   {
-    count[arr[i]]++;
+    count[rand() % 10000]++;
   }
   printMap(count, sizeof(count) / sizeof(int));
   return 0;
