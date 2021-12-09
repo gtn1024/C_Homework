6fc4153cdf933ba5b3b47088e36a34e06da60c93
Taoning Ge <gtn1024o@outlook.com>
2021-12-09 16:38:29 +0800

    update

--- 2021/Dec.9.Class/exp6_2.c
+++ 2021/Dec.9.Class/exp6_2.c
@@ -6,44 +6,58 @@ void swap3(int *, int *);
 void swap4(int *, int *);
 
 int main() {
-  int x, y;
+  int base1, base2;
   printf("Input 2 integers: ");
-  scanf("%i%i", &x, &y);
+  scanf("%i%i", &base1, &base2);
 
-  // swap1(x, y);
-  // printf("%i %i\n", x, y);
+  int x, y;
 
-  // swap2(&x, y);
-  // printf("%i %i\n", x, y);
+  x = base1;
+  y = base2;
+  swap1(x, y);
+  printf("%i %i\n\n", x, y);
 
+  x = base1;
+  y = base2;
+  swap2(&x, y);
+  printf("%i %i\n\n", x, y);
+
+  x = base1;
+  y = base2;
   swap3(&x, &y);
-  printf("%i %i\n", x, y);
+  printf("%i %i\n\n", x, y);
 
-  // swap4(&x, &y);
-  // printf("%i %i\n", x, y);
+  x = base1;
+  y = base2;
+  swap4(&x, &y);
+  printf("%i %i\n\n", x, y);
 
   return 0;
 }
 
 void swap1(int a, int b) {
+  printf("----- swap1 -----\n");
   int temp = a;
   a = b;
   b = temp;
 }
 
 void swap2(int *a, int b) {
+  printf("----- swap2 -----\n");
   int temp = *a;
   *a = b;
   b = temp;
 }
 
 void swap3(int *a, int *b) {
+  printf("----- swap3 -----\n");
   int temp = *a;
   *a = *b;
   *b = temp;
 }
 
 void swap4(int *a, int *b) {
+  printf("----- swap4 -----\n");
   int *temp = a;
   a = b;
   b = temp;
